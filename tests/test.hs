15da59ebb9d893f4e9de287ca4a9d702bae43873
Peter Andersson <sehnpaa@gmail.com>
2019-05-09 14:19:11 +0200

    Add addSecond test

--- tests/test.hs
+++ tests/test.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Lens
 import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Test.Tasty.QuickCheck as QC
@@ -19,3 +20,9 @@ tests = testGroup "Tests" [unitTests, qcTests]
 
 unitTests = testGroup "Unit tests"
   [ testCase "first" $ assertEqual "" (MyState defaultConfig [] [] 4 2 100 1 (IsStarted True)) (nextTick state1)]
+
+qcTests = testGroup "QuickCheck tests"
+  [ QC.testProperty "addSecond" $ QC.withMaxSuccess 1000 $ \state ->
+      let before = view seconds state
+          after = view seconds (addSecond state)
+          in after == before + 1]
