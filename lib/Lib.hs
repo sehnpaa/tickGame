961e07e4288f80ace90b3d79b4ed5c4b96cf3b59
Peter Andersson <sehnpaa@gmail.com>
2019-05-13 15:56:40 +0200

    Clean up buyHelper

--- lib/Lib.hs
+++ lib/Lib.hs
@@ -24,14 +24,46 @@ addSecond = over seconds succ
 createPC :: MyState -> MyState
 createPC = over (resources.paperclips) succ
 
+setO3 :: ASetter' s a -> ASetter' s b -> ASetter' s c -> s -> (a, b, c) -> s
+setO3 f1 f2 f3 s (a, b, c) = set f1 a $ set f2 b $ set f3 c s
+
 buyHelper :: MyState -> MyState
 buyHelper state =
-  let paperclips' = view (resources.paperclips) state
-      price = view (config.prices.helperPrices) state
-      s' = view seconds state
-    in if (unHelperPrice price) > paperclips'
-      then over errorLog (addToErrorLog (lineNeedMorePaperclips s')) state
-      else over (resources.helpers) succ $ over (resources.paperclips) (decPaperclipsWith price) state
+  setOutput state $ buyHelper' $ getInput state
+  where
+    getInput = getI5
+      seconds
+      (config.prices.helperPrices)
+      (resources.paperclips)
+      (resources.helpers)
+      errorLog
+    setOutput = setO3 (resources.paperclips) (resources.helpers) errorLog
+
+buyHelper'' :: Seconds -> HelperPrice -> Paperclips -> Helpers -> [ErrorLogLine] -> Either [ErrorLogLine] (Helpers, Paperclips)
+buyHelper'' s price p helpers log =
+  if unHelperPrice price > p
+    then Left $ addToErrorLog (lineNeedMorePaperclips s) log
+    else Right (succ helpers, decPaperclipsWith price p)
+
+getI5
+  :: Getting a s a
+  -> Getting b s b
+  -> Getting c s c
+  -> Getting d s d
+  -> Getting e s e
+  -> s
+  -> (a, b, c, d, e)
+getI5 f1 f2 f3 f4 f5 s =
+  ( view f1 s
+  , view f2 s
+  , view f3 s
+  , view f4 s
+  , view f5 s )
+
+buyHelper' :: (Seconds, HelperPrice, Paperclips, Helpers, [ErrorLogLine]) -> (Paperclips, Helpers, [ErrorLogLine])
+buyHelper' (s, hp, pc, helpers, errs )= case buyHelper'' s hp pc helpers errs of
+  Left errs' -> (pc, helpers, errs')
+  Right (hp', pc') -> (pc', hp', errs)
 
 decPaperclipsWith :: HelperPrice -> Paperclips -> Paperclips
 decPaperclipsWith price paperclips = paperclips - (unHelperPrice price)
