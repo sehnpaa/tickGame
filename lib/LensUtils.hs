e1264e90f945ce64074a091a9fa5758ef8eafa05
Peter Andersson <sehnpaa@gmail.com>
2019-05-14 08:40:03 +0200

    Add module LensUtils

---
+++ lib/LensUtils.hs
@@ -0,0 +1,57 @@
+module LensUtils where
+
+import Control.Lens
+
+getInput1 :: Getting a s a -> s -> a
+getInput1 = view
+
+getInput2 :: Getting a s a -> Getting b s b -> s -> (a, b)
+getInput2 f1 f2 s = (view f1 s, view f2 s)
+
+getInput3
+  :: Getting a s a
+  -> Getting b s b
+  -> Getting c s c
+  -> s
+  -> (a, b, c)
+getInput3 f1 f2 f3 s =
+  ( view f1 s
+  , view f2 s
+  , view f3 s )
+
+getInput4
+  :: Getting a s a
+  -> Getting b s b
+  -> Getting c s c
+  -> Getting d s d
+  -> s
+  -> (a, b, c, d)
+getInput4 f1 f2 f3 f4 s =
+  ( view f1 s
+  , view f2 s
+  , view f3 s
+  , view f4 s )
+
+getInput5
+  :: Getting a s a
+  -> Getting b s b
+  -> Getting c s c
+  -> Getting d s d
+  -> Getting e s e
+  -> s
+  -> (a, b, c, d, e)
+getInput5 f1 f2 f3 f4 f5 s =
+  ( view f1 s
+  , view f2 s
+  , view f3 s
+  , view f4 s
+  , view f5 s )
+
+setOutput1 :: ASetter' s a -> s -> a -> s
+setOutput1 f1 s a = set f1 a s
+
+setOutput2 :: ASetter' s a -> ASetter' s b -> s -> (a, b) -> s
+setOutput2 f1 f2 s (a, b) = set f1 a $ set f2 b s
+
+setOutput3 :: ASetter' s a -> ASetter' s b -> ASetter' s c -> s -> (a, b, c) -> s
+setOutput3 f1 f2 f3 s (a, b, c) = set f1 a $ set f2 b $ set f3 c s
--- lib/Lib.hs
+++ lib/Lib.hs
@@ -5,6 +5,7 @@ module Lib (module Lib, module Mod) where
 import Control.Lens
 import Data.Text (concat, pack)
 
+import LensUtils
 import Mod
 
 nextTick :: MyState -> MyState
@@ -24,41 +25,23 @@ addSecond = over seconds succ
 createPC :: MyState -> MyState
 createPC = over (resources.paperclips) succ
 
-setO3 :: ASetter' s a -> ASetter' s b -> ASetter' s c -> s -> (a, b, c) -> s
-setO3 f1 f2 f3 s (a, b, c) = set f1 a $ set f2 b $ set f3 c s
-
 buyHelper :: MyState -> MyState
 buyHelper state =
   setOutput state $ buyHelper' $ getInput state
   where
-    getInput = getI5
+    getInput = getInput5
       seconds
       (config.prices.helperPrices)
       (resources.paperclips)
       (resources.helpers)
       errorLog
-    setOutput = setO3 (resources.paperclips) (resources.helpers) errorLog
+    setOutput = setOutput3 (resources.paperclips) (resources.helpers) errorLog
 
 buyHelper'' :: Seconds -> HelperPrice -> Paperclips -> Helpers -> [ErrorLogLine] -> Either [ErrorLogLine] (Helpers, Paperclips)
 buyHelper'' s price p helpers log =
   if unHelperPrice price > p
     then Left $ addToErrorLog (lineNeedMorePaperclips s) log
     else Right (succ helpers, decPaperclipsWith price p)
-
-getI5
-  :: Getting a s a
-  -> Getting b s b
-  -> Getting c s c
-  -> Getting d s d
-  -> Getting e s e
-  -> s
-  -> (a, b, c, d, e)
-getI5 f1 f2 f3 f4 f5 s =
-  ( view f1 s
-  , view f2 s
-  , view f3 s
-  , view f4 s
-  , view f5 s )
 
 buyHelper' :: (Seconds, HelperPrice, Paperclips, Helpers, [ErrorLogLine]) -> (Paperclips, Helpers, [ErrorLogLine])
 buyHelper' (s, hp, pc, helpers, errs )= case buyHelper'' s hp pc helpers errs of
