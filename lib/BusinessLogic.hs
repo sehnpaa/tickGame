bfd2e46df9c76b6ad73f58dba1ea7b53a43fb07b
Peter Andersson <sehnpaa@gmail.com>
2019-05-20 11:18:24 +0200

    Clean up price unwrapping

--- lib/BusinessLogic.hs
+++ lib/BusinessLogic.hs
@@ -7,13 +7,10 @@ import Data.Text (concat, pack)
 import Mod
 
 buyHelper :: Seconds -> HelperPrice -> Paperclips -> Helpers -> [ErrorLogLine] -> Either [ErrorLogLine] (Helpers, Paperclips)
-buyHelper s price p helpers log =
-  if unHelperPrice price > p
+buyHelper s (HelperPrice price) p helpers log =
+  if price > p
     then Left $ addToErrorLog (lineNeedMorePaperclips s) log
     else Right (succ helpers, decPaperclipsWith price p)
-
-decPaperclipsWith :: HelperPrice -> Paperclips -> Paperclips
-decPaperclipsWith price paperclips = paperclips - (unHelperPrice price)
 
 addToErrorLog :: ErrorLogLine -> [ErrorLogLine] -> [ErrorLogLine]
 addToErrorLog new existing = existing ++ [new]
@@ -22,15 +19,15 @@ lineNeedMorePaperclips :: Seconds -> ErrorLogLine
 lineNeedMorePaperclips s = ErrorLogLine $ Data.Text.concat ["Tick ", pack (show s), ": You need more paperclips."]
 
 researchAdvancedHelper :: Seconds -> Paperclips -> AdvancedHelperPrice -> ResearchProgress -> Duration -> [ErrorLogLine] -> Either [ErrorLogLine] (Paperclips, ResearchProgress)
-researchAdvancedHelper s p price progress duration errs =
-  case ((unAdvancedHelperPrice price > p), progress) of
+researchAdvancedHelper s p (AdvancedHelperPrice price) progress duration errs =
+  case (price > p, progress) of
     (True, NotResearched) -> Left $ addToErrorLog (ErrorLogLine "Not enough paperclips") errs
-    (False, NotResearched) -> Right (decPaperclipsWith' (unAdvancedHelperPrice price) p, startResearch duration)
+    (False, NotResearched) -> Right (decPaperclipsWith price p, startResearch duration)
     (_, ResearchInProgress x) -> Left $ addToErrorLog (ErrorLogLine "Already in progress") errs
     (_, ResearchDone) -> Left $ addToErrorLog (ErrorLogLine "Already done") errs
 
-decPaperclipsWith' :: Paperclips -> Paperclips -> Paperclips
-decPaperclipsWith' price paperclips = paperclips - price
+decPaperclipsWith :: Paperclips -> Paperclips -> Paperclips
+decPaperclipsWith price paperclips = paperclips - price
 
 plantASeed :: Seconds -> TreePrice -> TreeSeeds -> Trees -> [ErrorLogLine] -> Either [ErrorLogLine] (TreeSeeds, Trees)
 plantASeed s price seeds trees errs =
