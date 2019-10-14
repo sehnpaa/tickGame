a533084db92c7002da7ee6edcf9bedc417c823d0
Peter Andersson <sehnpaa@gmail.com>
2019-10-14 15:04:26 +0200

    Use version 8.6.5 of ghc

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { nixpkgs ? import (fetchTarball {
     url = https://github.com/NixOS/nixpkgs-channels/archive/971b731fc18c86569211a460ef62e1d8001799e9.tar.gz;
     sha256 = "1b8xjrrwb8fz92bcrqvfvfg7gwn40ss12by2ka4yclcxk5yylmw0";
-  }) {} , compiler ? "ghc864", doBenchmark ? false }:
+  }) {} , compiler ? "ghc865", doBenchmark ? false }:
 
 let
 
