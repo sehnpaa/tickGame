7e10ff291897406c306838fdb03406c80f88e6f6
Peter Andersson <sehnpaa@gmail.com>
2019-12-14 15:21:10 +0100

    Make tradeoff limits optional

--- lib/Tradeoff.hs
+++ lib/Tradeoff.hs
@@ -24,8 +24,8 @@ data Tradeoff = Tradeoff
 
 data Resource = Resource
     { resourceName :: String
-    , resourceLowerLimit :: Integer
-    , resourceUpperLimit :: Integer } deriving (Eq, Ord, Show)
+    , resourceLowerLimit :: Maybe Integer
+    , resourceUpperLimit :: Maybe Integer } deriving (Eq, Ord, Show)
 
 replace :: (Eq a, Functor f) => a -> a -> f a -> f a
 replace x t = fmap (\c -> if c == x then t else c)
@@ -53,20 +53,24 @@ validateAbilities :: [Ability] -> Tradeoff -> Validation [Error] [Ability]
 validateAbilities as t =
     let xs = prerequisites t \\ as
     in  if xs == []
-            then Success (addAbilities as (achieveAbilities t))
-            else Failure [MissingAbilities xs]
+        then Success (addAbilities as (achieveAbilities t))
+        else Failure [MissingAbilities xs]
 
 validateCapitalLowerLimitSingle
     :: (Resource, Amount) -> Validation [Error] [(Resource, Amount)]
 validateCapitalLowerLimitSingle (r, a) =
-    let limit = resourceLowerLimit r
-    in  if a < limit then Failure [CapitalTooLow r a] else Success [(r, a)]
+    let mayLimit = resourceLowerLimit r in
+    case mayLimit of
+    Nothing -> Success [(r,a)]
+    Just limit -> if a < limit then Failure [CapitalTooLow r a] else Success [(r, a)]
 
 validateCapitalUpperLimitSingle
     :: (Resource, Amount) -> Validation [Error] [(Resource, Amount)]
 validateCapitalUpperLimitSingle (r, a) =
-    let limit = resourceUpperLimit r
-    in  if a > limit then Failure [CapitalTooHigh r a] else Success [(r, a)]
+    let mayLimit = resourceUpperLimit r in
+    case mayLimit of
+    Nothing -> Success [(r,a)]
+    Just limit -> if a > limit then Failure [CapitalTooHigh r a] else Success [(r, a)]
 
 rules :: [(Resource, Amount) -> Validation [Error] [(Resource, Amount)]]
 rules = [validateCapitalUpperLimitSingle, validateCapitalLowerLimitSingle]
@@ -77,19 +81,33 @@ validateRA = mergeSuccessConjunction . nub . (<$> rules) . (&)
 validateTransfer :: Capital -> Validation [Error] Capital
 validateTransfer = mergeSuccessConjunction . map validateRA
 
+mergeSuccessDisjunction
+    :: [Validation [Error] Capital] -> Validation [Error] Capital
+mergeSuccessDisjunction = foldr mappendDisjunction (Success [])
+
 mergeSuccessConjunction
     :: [Validation [Error] Capital] -> Validation [Error] Capital
-mergeSuccessConjunction = foldr mappend2 (Success [])
+mergeSuccessConjunction = foldr mappendConjunction (Success [])
 
--- For Success, require that none is Failure
-mappend2
+-- For Success, require that any is Success
+mappendDisjunction
     :: Validation [Error] Capital
     -> Validation [Error] Capital
     -> Validation [Error] Capital
-mappend2 (Failure a) (Failure b) = Failure $ mappend a b
-mappend2 (Failure a) (Success _) = Failure a
-mappend2 (Success _) (Failure b) = Failure b
-mappend2 (Success a) (Success b) = Success $ mappend a b
+mappendDisjunction (Failure a) (Failure b) = Failure $ mappend a b
+mappendDisjunction (Failure _) (Success b) = Success b
+mappendDisjunction (Success a) (Failure _) = Success a
+mappendDisjunction (Success a) (Success b) = Success $ mappend a b
+
+-- For Success, require that none is Failure
+mappendConjunction
+    :: Validation [Error] Capital
+    -> Validation [Error] Capital
+    -> Validation [Error] Capital
+mappendConjunction (Failure a) (Failure b) = Failure $ mappend a b
+mappendConjunction (Failure a) (Success _) = Failure a
+mappendConjunction (Success _) (Failure b) = Failure b
+mappendConjunction (Success a) (Success b) = Success $ mappend a b
 
 tryTransfer :: Capital -> Tradeoff -> Capital
 tryTransfer rs t = removeCapital (addCapital rs (gains t)) (losses t)
